32da5a922fce0464eb92ebb8a1c3a040530046a5
Jared Rathbun <rathbunj@merrimack.edu>
2022-09-08 21:43:57 -0400

    Added Spring Boot starter project

---
+++ src/main/java/com/teammspaint/stemdatadashboard/StemdatadashboardApplication.java
@@ -0,0 +1,13 @@
+package com.teammspaint.stemdatadashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StemdatadashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StemdatadashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teammspaint/stemdatadashboard/StemdatadashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teammspaint.stemdatadashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemdatadashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
