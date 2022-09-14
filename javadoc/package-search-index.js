b563769f7a889da39d96be23b5091a712820957b
JaredRathbun <72120661+JaredRathbun@users.noreply.github.com>
2022-09-14 01:46:49 +0000

    Deploying to javadoc from @ JaredRathbun/TeamMicrosoftPaint@3587df7bac2c58021c297a430bdac53297cddba7 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.teammspaint.stemdatadashboard"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.teammspaint.stemdatadashboard"},{"l":"com.teammspaint.stemdatadashboard.controller"}];updateSearchResults();
--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.teammspaint.stemdatadashboard","l":"Application"},{"p":"com.teammspaint.stemdatadashboard","l":"SQLDialect"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.teammspaint.stemdatadashboard","l":"Application"},{"p":"com.teammspaint.stemdatadashboard.controller","l":"Endpoints"},{"p":"com.teammspaint.stemdatadashboard","l":"SQLDialect"}];updateSearchResults();
